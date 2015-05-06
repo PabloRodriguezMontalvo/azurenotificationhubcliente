f75a5352c29ed1b1a105b63e0356955fac2fa59c
luisgiltajamar <luis.gil@tajamar365.com>
2015-05-06 12:14:52 +0200

    A

---
+++ TestNot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
+
+namespace TestNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendNotificationAsync()
+        {
+            var t = new List<String>() { "Usuario:" + 77 };
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString("Endpoint=sb://testnh2-ns.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=5Sot8Cy4PRmSzjYR4BBxAded6FALlJN0sDc+Nl62nXQ=", "testnh");
+            var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">Hello from a .NET App!</text></binding></visual></toast>";
+          var r=  await hub.SendWindowsNativeNotificationAsync(toast,t);
+
+            var x = r.Results;
+        }
+    }
+}
